f11ab87be3276e86859ca20eba0595db1e5f076f
mattNat <mattsanpedro76@gmail.com>
2018-05-23 14:24:35 -0700

    clean cod in search_bar

--- client/src/containers/search_bar.js
+++ client/src/containers/search_bar.js
@@ -16,29 +16,7 @@ class SearchBar extends Component {
     this.state = {
       address: ''
     };
-
-    // this (which is an instance of search bar)
-    // this.onInputChange = this.onInputChange.bind(this);        
-    // this.onFormSubmit = this.onFormSubmit.bind(this);
   }
-
-  // onInputChange(event) {
-  //   // this.setState (I don't have it)
-  //   this.setState({address: event.target.value});
-  // }
-
-
-  // onFormSubmit(event) {
-  //   event.preventDefault();
-
-  //   // // We need to go and fetch weather data
-  //   // this.props.fetchTrails(this.state.lat, this.state.lon);
-
-  //   // We need to go and fetch coordinates data
-  //   this.props.fetchCoordinates(this.state.address);
-  //   // clear search input, will pass it to value in input
-  //   // this.setState({ address: '' });
-  // }
 
   // Google autocomplete functions
   handleChange = (address) => {
@@ -67,45 +45,30 @@ class SearchBar extends Component {
             onChange={this.handleChange}
             onSelect={this.handleSelect}
           >
-          {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-            <div>
-              <input
-                {...getInputProps({
-                  placeholder: 'Search Places ...',
-                  className: 'location-search-input',
-                  // value: {this.state.address},
-                  // onChange: {this.onInputChange}
-                })}
-              />
-              <div className="autocomplete-dropdown-container">
-                {suggestions.map(suggestion => {
-                  const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                  // inline style for demonstration purpose
-                  const style = suggestion.active
-                              ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                              : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                  return (
-                    <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                      <span>{suggestion.description}</span>
-                    </div>
-                  )
-                })}
+            {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: 'Search Places ...',
+                    className: 'location-search-input',
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                        <span>{suggestion.description}</span>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
-            </div>
-          )}
-          {/* <form onSubmit={this.onFormSubmit} className='input-group'>
-            <input 
-              placeholder='Address or location...'
-              className='searchTerm'
-              value={this.state.address}
-              onChange={this.onInputChange}
-            />
-            <span className='input-group-btn'>
-              <button type='submit' className='searchButton'>
-                <i className="fa fa-search"></i>
-              </button>
-            </span>
-          </form> */}
+            )}
           </PlaceAutoComplete>
         </div>
       </div>
