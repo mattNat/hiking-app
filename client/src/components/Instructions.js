661aad5885e873647cac0cd5906157d4d1779686
mattNat <mattsanpedro76@gmail.com>
2018-05-23 01:48:16 -0700

    continue adding content to instructions.js

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -4,8 +4,13 @@ export default function Instructions() {
   return (
     <div className='instructions'>
       <h1>Hiking Friend</h1>
+      <h2>Core Functionality</h2>
       <p>
-        Hiking Friend is a personal app.
+        Hiking Friend is a searching app for hiking trails.  It's core feature includes Google Geolocation and Hiking Project APIs to return 10 popular trails.  After this search, the user is free to store trails for future reference.
+      </p>
+      <h2>Instructions</h2>
+      <p>
+        To begin searching for your favorite trails, navigate to the search web page by clicking on "SEARCH TRAILS" on the navigation bar.  
       </p>
     </div>
   );
